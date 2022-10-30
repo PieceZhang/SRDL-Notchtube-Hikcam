12abb1027466c352b200be8b20a1bd8b1ffe55fe
YuelinZhang <2239436414@qq.com>
2022-10-30 20:13:51 +0800

    update callback

--- My_utils/utils_callback.py
+++ My_utils/utils_callback.py
@@ -1,8 +1,13 @@
 import numpy as np
+import cv2
 
 
 def Hikcam_callback(camnum: int, im: np.ndarray):
-    print("[INFO] My Callback: Cam {}, frame {}".format(camnum, im.shape))
     # TODO 统计帧率
     # TODO 添加功能
-    return im
+    print("[INFO: CAM{}] My Callback: Cam {}, frame {}".format(camnum, camnum, im.shape))
+    cv2.imshow("camera{}".format(camnum), cv2.resize(im, (300, 200), interpolation=cv2.INTER_CUBIC))
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        return im, False
+    return im, True
